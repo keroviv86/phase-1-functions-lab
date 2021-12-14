fafb45d9b9955ec2a5c937d26ec9e3ca5c6e8c2e
Vivian Yim <keroviv86@gmail.com>
2021-12-14 08:05:17 -0600

    commit tree 906069e9af98ec7583cdedf23b10d6544fb470dc

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42-someValue)
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)* 264
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination) *264
+    //returns the number of feet traveled
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let distance = Math.abs(start-destination) *264
+    if(distance <= 400){
+        return 0
+    }
+    if(distance > 400 && distance <2000){
+        return 0.02* (distance-400)
+
+    }else if(distance >2000 && distance <2500){
+        return 25
+        
+    }else if(distance > 2500){
+        return 'cannot travel that far'
+    }
+  }
